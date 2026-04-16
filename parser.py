2256440f44efb454f5312ffd118a7053a3639b5e
ivan-zotov <2ivanzotov@gmail.com>
2026-04-16 15:06:50 +0300

    fix By import

--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@ import time
 import json
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
